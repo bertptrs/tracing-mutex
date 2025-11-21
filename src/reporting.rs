fa73bd760c028e355bf5e88967ec66f361d16322
Adam Perry <adamperry@google.com>
2025-11-21 18:25:30 +0000

    Allow suppressing panics.
    This makes it possible to incrementally adopt tracing-mutex
    in a library shared by many binaries in the same project without
    having to fix all potential deadlocks in all binaries at once. The
    binaries that don't yet have proper lock hygeine can opt-out of
    real enforcement until their deadlocks are addressed.

--- src/reporting.rs
+++ src/reporting.rs
@@ -6,6 +6,7 @@ use std::backtrace::Backtrace;
 use std::borrow::Cow;
 use std::fmt::Write;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 #[cfg(feature = "backtraces")]
 pub type Dep = MutexDep<Arc<Backtrace>>;
@@ -15,12 +16,32 @@ pub type Dep = MutexDep<()>;
 // Base message to be reported when cycle is detected
 const BASE_MESSAGE: &str = "Found cycle in mutex dependency graph:";
 
+static SHOULD_PANIC: AtomicBool = AtomicBool::new(true);
+
+/// Call this early in main() to suppress panics when a cycle is detected and print the lock cycles
+/// to stderr instead.
+///
+/// This is not as useful a mechanism for detecting issues as a panic, but it is useful for
+/// incrementally rolling out usage of tracing-mutex to many programs that use a shared crate.
+pub fn suppress_panics() {
+    SHOULD_PANIC.store(false, Ordering::Relaxed);
+}
+
+pub(crate) fn report_cycle(cycle: &[Dep]) {
+    let message = Dep::message(cycle);
+    if SHOULD_PANIC.load(Ordering::Relaxed) {
+        panic!("{message}");
+    } else {
+        eprintln!("{message}");
+    }
+}
+
 pub trait Reportable: Clone {
     /// Capture the current state
     fn capture() -> Self;
 
     /// Format a trace of state for human readable consumption.
-    fn panic_message(trace: &[Self]) -> Cow<'static, str>;
+    fn message(trace: &[Self]) -> Cow<'static, str>;
 }
 
 #[derive(Clone)]
@@ -35,7 +56,7 @@ impl Reportable for MutexDep<()> {
         Self(())
     }
 
-    fn panic_message(_trace: &[Self]) -> Cow<'static, str> {
+    fn message(_trace: &[Self]) -> Cow<'static, str> {
         Cow::Borrowed(BASE_MESSAGE)
     }
 }
@@ -52,7 +73,7 @@ impl Reportable for MutexDep<Arc<Backtrace>> {
         Self(Arc::new(Backtrace::capture()))
     }
 
-    fn panic_message(trace: &[Self]) -> Cow<'static, str> {
+    fn message(trace: &[Self]) -> Cow<'static, str> {
         let mut message = format!("{BASE_MESSAGE}\n");
 
         for entry in trace {
